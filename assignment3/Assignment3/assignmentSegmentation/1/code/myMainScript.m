72d69c41a004faeb18292c63a83af9612cce51f5
SudhirSuman <alokiitb11@gmail.com>
2018-10-14 19:16:54 +0000

    after assignment3

--- assignment3/Assignment3/assignmentSegmentation/1/code/myMainScript.m
+++
@@ -1,9 +0,0 @@
-%% MyMainScript
-
-tic;
-%% Your code here
-myHarrisCornerDetector;
-% Threshod = 15*avg pixel intensity of H
-% Standard deviation of gaussian  = 2
-% Emperical tuned constant k = 0.05
-toc;
